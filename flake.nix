e0d5ec6cced2ae2800a7430ff41bc4f91c6c9af8
hugosenari <hugosenari@gmail.com>
2022-03-12 00:43:54 -0300

    chore: re index basedon

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A library for cleanly creating an object or tuple based on another object or tuple'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."basedon-master".url = "path:./master";
+  inputs."basedon-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."basedon-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
